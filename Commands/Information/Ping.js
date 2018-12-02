4d7e1c4d93cb0b1fabf7a96cac403b2160c610ce
Beta <21072460+bvta@users.noreply.github.com>
2018-12-02 15:19:17 -0600

    noah, make use of await and async <3

--- Commands/Information/Ping.js
+++ Commands/Information/Ping.js
@@ -1,19 +1,18 @@
 const { Command } = require("discord-akairo")
 
 class Ping extends Command {
-    constructor() {
-        super("ping", {
-            aliases: ["ping"]
-        })
-    }
+	constructor() {
+		super("ping", {
+			aliases: ["ping"]
+		})
+	}
 
-    exec(message) {
-        const startTime = Date.now();
-        message.channel.send(`Pinging...`).then(msg => {
-            const endTime = Date.now();
-            msg.edit(`Pong! | Client Ping: ${endTime - startTime}ms | API Latency: ${Math.floor(this.client.pings[0])}ms`);
-        });
-    }
+	async exec(message) {
+		const startTime = Date.now();
+		m = await message.channel.send(`Pinging...`)
+		const endTime = Date.now();
+		m.edit(`Pong! | Client Ping: ${endTime - startTime}ms | API Latency: ${Math.floor(this.client.pings[0])}ms`);
+	}
 }
 
 module.exports = Ping;
